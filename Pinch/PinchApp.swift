d7e0be3855ca3b32c3d561e367430c19ab45b4fd
Gabriel Orlando <gabriel.orlando00@gmail.com>
2024-06-12 08:19:45 -0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Gabriel Orlando on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
